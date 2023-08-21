7803fbb8983ee53b699ac4771f1eb4568c39506b
Pieandmilk <142815776+Pieandmilk@users.noreply.github.com>
2023-08-21 23:06:00 +0800

    Square.cpp

--- Square.cpp
+++ Square.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
     int size = 30; // Adjust the size of the square
-    std::string word = "CUTE";
+    string word = "CUTE";
 
     if (size < word.length()) {
         std::cerr << "Size is too small for the word." << std::endl;
         return 1;
     }
 
-    char topLeft = 201;     // +
-    char topRight = 187;    // +
-    char bottomLeft = 200;  // +
-    char bottomRight = 188; // +
-    char horizontal = 205;  // -
-    char vertical = 186;    // ¦
+    char topLeft = 201;     
+    char topRight = 187;    
+    char bottomLeft = 200;  
+    char bottomRight = 188; 
+    char horizontal = 205;  
+    char vertical = 186;    
 
     // Print the top of the square
-    std::cout << topLeft;
+    cout << topLeft;
     for (int i = 0; i < size - 2; ++i)
-        std::cout << horizontal;
-    std::cout << topRight << std::endl;
+        cout << horizontal;
+    cout << topRight << endl;
 
     // Print the sides of the square
     for (int i = 0; i < size - 2; ++i) {
-        std::cout << vertical;
+        cout << vertical;
         for (int j = 0; j < size - 2; ++j) {
             if (i == (size - 2) / 2 && j == (size - word.length()) / 2) {
-                std::cout << word;
+                cout << word;
                 j += word.length() - 1;
             } else {
-                std::cout << " ";
+                cout << " ";
             }
         }
-        std::cout << vertical << std::endl;
+        cout << vertical << endl;
     }
 
     // Print the bottom of the square
-    std::cout << bottomLeft;
+    cout << bottomLeft;
     for (int i = 0; i < size - 2; ++i)
-        std::cout << horizontal;
-    std::cout << bottomRight << std::endl;
+        cout << horizontal;
+    cout << bottomRight << endl;
 
     return 0;
 }
-
