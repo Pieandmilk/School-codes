b84d7d1e5034fc2367791961772857b8de6e7895
Pieandmilk <142815776+Pieandmilk@users.noreply.github.com>
2023-08-24 00:03:03 +0800

    Square_V2.cpp

--- square2.0.cpp
+++ square2.0.cpp
@@ -1,64 +1,56 @@
-//ELEGADO
-
 #include <iostream>
 #include <string>
-#include<windows.h>
+#include <windows.h> 
 
 using namespace std;
 
-void gotoxy(short x, short y);
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 int main() {
+    string word = "CUTIE";
+    int wordLength = word.length();
 
-    string word = "FIVE";
-	int size =(word.length()*2);
-
+    int boxWidth = wordLength + 2; // Adjusted box width
 
     char topLeft = 201;     
     char topRight = 187;    
     char bottomLeft = 200;  
     char bottomRight = 188; 
     char horizontal = 205;  
-    char vertical = 186;    
+    char vertical = 186;   
 
     // Print the top of the square
+    gotoxy(1, 1);
     cout << topLeft;
-    for (int i = 0; i < size - 2; ++i)
+    for (int i = 0; i < boxWidth ; ++i)
         cout << horizontal;
     cout << topRight << endl;
-    
 
-	
     // Print the sides of the square
-    for (int i = 0; i < size - 2; ++i) {
-        cout << vertical;
-        for (int j = 0; j < size - 2; ++j) 
-		{
-			// Print the word
-	int x=size/2,y=size/2;
-	gotoxy(x,y);
-	cout<<word;
-	return 0;
-		cout << " ";
-            
+    gotoxy(1, 2);
+    cout << vertical;
+    for (int j = 0; j < boxWidth ; ++j) {
+        if (j == 1) {
+            cout << word;
+            j += wordLength - 1;
+        } else {
+            cout << " ";
         }
-        cout << vertical << endl;
     }
+    cout << vertical << endl;
 
-	
     // Print the bottom of the square
+    gotoxy(1, 3);
     cout << bottomLeft;
-    for (int i = 0; i < size - 2; ++i)
+    for (int i = 0; i < boxWidth ; ++i)
         cout << horizontal;
     cout << bottomRight << endl;
 
     return 0;
-    
-
-
-}
-void gotoxy(short x, short y)           //definition of gotoxy function//                                               
-{
- COORD pos ={x,y};
- SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
